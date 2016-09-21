077562262879010321b9889bf17d96da5b70c7ef
wubiao <wubiao0313@163.com>
2016-09-21 14:09:19 +0800

    Initial Commit

---
+++ WBCustomSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WBCustomSDK
+//
+//  Created by 吴飚 on 16/9/21.
+//  Copyright © 2016年 wubiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WBCustomSDK/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WBCustomSDK
+//
+//  Created by 吴飚 on 16/9/21.
+//  Copyright © 2016年 wubiao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
